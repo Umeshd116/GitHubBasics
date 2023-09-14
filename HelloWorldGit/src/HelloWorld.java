89097cc9d435746d14f023db9e996ad6f83b1a30
Umesh <19bcs2532@gmail.com>
2023-09-15 01:23:54 +0530

    Second Commit

---
+++ HelloWorldGit/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("Hello World" );
+    }
+
+}
+/* For a basic idea on how git works, I used java (Intellij) to push a program. {master branch} */
